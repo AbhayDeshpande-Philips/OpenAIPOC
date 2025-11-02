4971f28c6be4e3e2f416696ba0000deb5dd90600
Abhay Deshpande <abhay.deshpande@philips.com>
2025-11-02 23:53:28 +0530

    Updated the placeholder text

--- lib/config.ts
+++ lib/config.ts
@@ -3,15 +3,9 @@ import { StartScreenPrompt } from "@openai/chatkit";
 export const WORKFLOW_ID = process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() ?? "";
 
 
-export const STARTER_PROMPTS: StartScreenPrompt[] = [
-  {
-    label: "What can you do?",
-    prompt: "What can you do?",
-    icon: "circle-question",
-  },
-];
+export const STARTER_PROMPTS: StartScreenPrompt[] = [];
 
-export const PLACEHOLDER_INPUT = "test 123";
+export const PLACEHOLDER_INPUT = "";
 
 export const GREETING = "How can I help you today?";
 
